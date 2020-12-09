7ab9253cbdd85b0ec8af487679f42d825b6020dc
Andreu <anddessol@gmail.com>
2020-12-10 00:08:14 +0100

    feat: scroll to video

--- src/js/main.js
+++ src/js/main.js
@@ -4,21 +4,32 @@ const player = {
   tags: document.getElementById("tags")
 };
 
-document
-  .querySelectorAll(".js-song")
-  .forEach(node => node.addEventListener("click", displayVideo));
+document.querySelectorAll(".js-song").forEach(node => {
+  node.addEventListener("click", e => {
+    displayVideo(e);
+    displayInfo(e);
+    scrollToPlayer();
+  });
+});
 
 function displayVideo(e) {
+  console.log(e.currentTarget.children[1].children[0].children[0].innerText);
   document.getElementById(
     "ytplayer"
   ).src = `https://www.youtube.com/embed/${e.currentTarget.dataset.videoid}`;
+}
 
+function displayInfo(e) {
   const selectedSong = {
-    title: e.currentTarget.children[1].children[0].innerText,
-    artist: e.currentTarget.children[1].children[1].innerText,
-    tags: e.currentTarget.children[1].children[2].innerText
+    title: e.currentTarget.children[1].children[0].children[0].innerText,
+    artist: e.currentTarget.children[1].children[0].children[1].innerText,
+    tags: e.currentTarget.children[1].children[0].children[2].innerText
   };
   player.title.innerText = selectedSong.title;
   player.artist.innerText = selectedSong.artist;
   player.tags.innerText = selectedSong.tags;
+}
+
+function scrollToPlayer() {
+  document.getElementById("ytplayer").scrollIntoView({ behavior: "smooth" });
 }
